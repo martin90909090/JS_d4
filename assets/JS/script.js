8228cae0c26cfebcd4f5de487ba399c4bb649423
martin90909090 <personalmperez@gmail.com>
2024-01-10 08:40:18 -0300

    m

---
+++ assets/JS/script.js
@@ -0,0 +1,135 @@
+//Propiedades
+
+//Propiedades en venta
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_2 = {
+    name: 'Apartamento acogedor en la montaña',
+    description: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+    address: '789 Mountain Road, Summit Peaks, CA 23456',
+    rooms: 2,
+    bathrooms: 1,
+    somke: true,
+    pets: true,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+//Propiedades en alquiler
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
+
+let propiedad_1 = {
+    name: 'Apartamento de lujo en zona exclusiva',
+    description: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    address: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    rooms: 4,
+    bathrooms: 4,
+    somke: false,
+    pets: false,
+    img: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg'
+};
